4e865ee1c0c2ef1124c299c5d316ae2ee9c5aa38
alda27 <amunozro8970@universidadean.edu.co>
2020-05-17 01:42:21 -0500

    Updates

---
+++ precache-manifest.b43aa6a14d40f7acc9c8402ca015b59a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c98784a959ccec316083a47c87b733f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ca7d4ab4e774d845357f",
+    "url": "/monster-rolodex/static/css/main.e03bcdd1.chunk.css"
+  },
+  {
+    "revision": "82815bfcb9bf73d8a971",
+    "url": "/monster-rolodex/static/js/2.be6af004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.be6af004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7d4ab4e774d845357f",
+    "url": "/monster-rolodex/static/js/main.f7f88d56.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
